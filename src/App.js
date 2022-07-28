8d2df0a3c7b357c1394e0b467616ae9f6150ce17
reza2boyce <reza2boyce@gmail.com>
2022-07-28 13:34:00 +0430

    seprated syles files

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import Home from './pages/Home';
+import ProductList from './pages/ProductList';
 
 const App = () => {
     return (
         <div className="App">
-            <Home />
+            <ProductList />
         </div>
     );
 }
---
+++ src/pages/ProductList.js
@@ -0,0 +1,11 @@
+import './ProductList.css';
+
+const ProductList = () => {
+    return (  
+        <div className="product-list">
+            <div>this is productlist</div>
+        </div>
+    );
+}
+ 
+export default ProductList;
